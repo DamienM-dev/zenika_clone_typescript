013e1470426223ee92a56666ca1d283c572faaae
Damien MIREMONT <damien.miremont@gmail.com>
2023-10-19 09:29:47 +0200

    MODIFICATION footer

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,11 +1,27 @@
 import Social from "./social";
 import Mention from "./mention";
+import Image from "next/image";
+
+const LOGOWHITE = "images/logo_light.svg";
+const ALT = "Logo de Zenika";
+const COPYRIGHT =
+  "Ceci est en but d'entraiment − Tous droits réservés à Zenika.";
 
 function Footer() {
   return (
-    <footer className="pt-9z">
-      <Social />
-      <Mention />
+    <footer>
+      <div className="mx-auto max-w-6xl">
+        <div className="pt-9 lg:grid lg:grid-cols-3">
+          <div className="flex justify-center p-2 lg:justify-start">
+            <Image src={LOGOWHITE} alt={ALT} width={165} height={52} />
+          </div>
+          <Social />
+          <Mention />
+        </div>
+        <div className="py-8 text-center">
+          <p>{COPYRIGHT}</p>
+        </div>
+      </div>
     </footer>
   );
 }
--- src/components/Footer/social.tsx
+++ src/components/Footer/social.tsx
@@ -8,10 +8,6 @@ interface Social {
   titre: string;
   lien: string;
 }
-
-const LOGOWHITE = "images/logo_light.svg";
-const ALT = "Logo de Zenika";
-
 function Social() {
   const [sociaux, setSociaux] = useState<Social[]>([]);
   const [error, setError] = useState(null);
@@ -39,13 +35,9 @@ function Social() {
   }
 
   return (
-    <div>
-      <div className="flex flex-col">
-        <div className="m-auto p-2">
-          <Image src={LOGOWHITE} alt={ALT} width={165} height={52} />
-        </div>
-
-        {sociaux.map((social) => (
+    <div className="flex flex-col lg:flex-row lg:justify-between">
+      <div className="">
+        {sociaux.slice(0, 3).map((social) => (
           <ul key={social.id} className="pt-2">
             <li className="flex justify-center p-2">
               <a href={social.lien} className="flex">
@@ -56,7 +48,25 @@ function Social() {
                   height={20}
                   className="invert filter"
                 />
-                <h3 className="ps-1">{social.titre}</h3>
+                <h3 className="ps-1 font-nunito-light">{social.titre}</h3>
+              </a>
+            </li>
+          </ul>
+        ))}
+      </div>
+      <div className="">
+        {sociaux.slice(3, 7).map((social) => (
+          <ul key={social.id} className="pt-2">
+            <li className="flex justify-center p-2">
+              <a href={social.lien} className="flex">
+                <Image
+                  src={social.logo}
+                  alt={social.alt}
+                  width={20}
+                  height={20}
+                  className="invert filter"
+                />
+                <h3 className="ps-1 font-nunito-light">{social.titre}</h3>
               </a>
             </li>
           </ul>
