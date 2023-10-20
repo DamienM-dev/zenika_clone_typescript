dbe910ba2b419eea0d7c69af81c99cf73692a565
Damien MIREMONT <damien.miremont@gmail.com>
2023-10-20 14:13:18 +0200

    DEBUT menu responsive

--- src/components/Header/menu.tsx
+++ src/components/Header/menu.tsx
@@ -1,6 +1,6 @@
-import { link } from "fs";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 //navigation lien
 const MENU = "/icone/menu.png";
 const ALTMENU = " Menu de navigation";
@@ -11,7 +11,7 @@ type LinkType = {
 };
 
 const LINKS: LinkType[] = [
-  { name: "valeur", link: "/valeurs" },
+  { name: "valeurs", link: "/valeurs" },
   { name: "offres", link: "/offres" },
   { name: "green it", link: "/green-it" },
   { name: "sécurité", link: "/securite" },
@@ -26,10 +26,60 @@ const LINKSSECONDARY: LinkType[] = [
 const CONTACT: LinkType[] = [{ name: "contact", link: "/contact" }];
 
 function Menu() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="self-center">
-      <div className="xl:hidden">
+      <div
+        className="absolute right-8 top-3 h-7 w-7 cursor-pointer xl:hidden"
+        onClick={() => setOpen(!open)}
+      >
         <Image src={MENU} alt={ALTMENU} height={36} width={30} />
+        {open ? (
+          // ---------- MENU PETIT ECRAN ----------
+          <div
+            className={`duration-900 fixed right-0 top-0 z-50 h-screen w-80 transform bg-white px-6 pt-10 text-center transition-transform ease-out ${
+              open ? "translate-x-0" : "translate-x-full"
+            }`}
+          >
+            <div className="p-4">
+              <ul className=" ">
+                {LINKS.map((link) => (
+                  <li
+                    key={link.name}
+                    className="mr-8 mt-6 font-nunito-light text-xl"
+                  >
+                    <Link className="uppercase" href={link.link}>
+                      {link.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <div className="mb-6 block h-14"></div>
+              <ul className=" ">
+                {LINKSSECONDARY.map((linkSecondary) => (
+                  <li
+                    key={linkSecondary.name}
+                    className="mr-8 mt-6 font-nunito-light text-xl"
+                  >
+                    <Link className="uppercase" href={linkSecondary.link}>
+                      {linkSecondary.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="mb-6 block h-72"></div>
+            <div>
+              {CONTACT.map((contact) => (
+                <a key={contact.name} href={contact.link}>
+                  {contact.name}
+                </a>
+              ))}
+            </div>
+          </div>
+        ) : null}
+        {/* ---------- MENU GRAND ECRAN ---------- */}
       </div>
       <div className="hidden xl:flex">
         <ul className=" xl:flex">
